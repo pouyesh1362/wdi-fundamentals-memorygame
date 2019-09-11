1b845e55187ecf6e289278b0248a67debe144728
pouyesh <pouyesh1362@hotmail.com>
2019-09-10 19:07:17 -0700

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,11 @@
+//console.log("Up and running!");
+
+var cardOne = "queen";
+var cardTwo = cardOne;
+var cardThree = "king";
+var cardFour = cardThree;
+
+console.log("user Flipped " + cardOne);
+console.log("user Flipped " + cardTwo);
+console.log("user Flipped " + cardThree);
+console.log("user Flipped " + cardFour);
